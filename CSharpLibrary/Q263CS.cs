d22adfc04fd6c152bdb35c1158d546ff2c0cbf0c
fo40225 <fo40225@hotmail.com>
2015-05-23 17:51:31 +0800

    Q263 csharp all test passed

--- CSharpLibrary/Q263CS.cs
+++ CSharpLibrary/Q263CS.cs
@@ -16,7 +16,41 @@ namespace CSharpLibrary
     {
         public static void GetAnswer(int input)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Original number was {0}", input.ToString());
+            HashSet<int> set = new HashSet<int>() { input };
+            int max;
+            int min;
+            int ans;
+            do
+            {
+                max = GetMax(input);
+                min = GetMin(input);
+                ans = max - min;
+                Console.WriteLine("{0} - {1} = {2}", max.ToString(), min.ToString(), ans.ToString());
+            } while (((Func<bool>)(() =>
+            {
+                if (set.Contains(ans))
+                {
+                    return false;
+                }
+                else
+                {
+                    set.Add(ans);
+                    input = ans;
+                    return true;
+                }
+            }))());
+            Console.WriteLine("Chain length {0}", set.Count.ToString());
+        }
+
+        private static int GetMin(int i)
+        {
+            return Convert.ToInt32(string.Concat(i.ToString().OrderBy(x => Convert.ToInt32(x))));
+        }
+
+        private static int GetMax(int i)
+        {
+            return Convert.ToInt32(string.Concat(i.ToString().OrderByDescending(x => Convert.ToInt32(x))));
         }
     }
 }
