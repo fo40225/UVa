498bdb9102dc530331a4f832e708c21dd6626811
fo40225 <fo40225@hotmail.com>
2015-05-23 17:41:59 +0800

    Add unit test project

---
+++ UnitTestProject/TestTemplate.cs
@@ -0,0 +1,30 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="$$Tests.cs">
+//     Copyright (c) Ching Hsu. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace UnitTestProject
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class $$Tests
+    {
+        [TestMethod]
+        public void CSTestMethod1()
+        {
+            //arrange
+            var input ;
+            var expected ;
+
+            //act
+            var actual = $$CS.GetAnswer(input);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
