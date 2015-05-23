0eeb2651d3eb2cd5b1e2573363bc94d5073a90e7
fo40225 <fo40225@hotmail.com>
2015-05-23 19:02:35 +0800

    Q161 fsharp some minor  refactoring

--- FSharpLibrary/Q161FS.fs
+++ FSharpLibrary/Q161FS.fs
@@ -6,11 +6,11 @@ type private LightStatus =
     | Red = 2
 
 module Q161FS = 
-    let private trafficLight period nowSec = 
+    let private TtrafficLightStatus period nowSec = 
         let greenToYellow p = p - 5
         let yellowToRed p = p
         let redToGreen p = p * 2
-        let GetInCycleSec input = input % redToGreen period
+        let getInCycleSec input = input % redToGreen period
         
         let inCycleSecToStatus sec = 
             match sec with
@@ -19,23 +19,21 @@ module Q161FS =
             | x when greenToYellow period < x && x <= yellowToRed period -> LightStatus.Yellow
             | x when yellowToRed period < x || x = 0 -> LightStatus.Red
             | _ -> failwith "error"
-        
-        let inCycleSec = GetInCycleSec nowSec
-        inCycleSecToStatus inCycleSec
+        getInCycleSec nowSec |> inCycleSecToStatus
     
     let rec private FindNextAllGreen periods passedSec = 
         if periods
-           |> Array.map (fun x -> trafficLight x passedSec)
+           |> Array.map (fun x -> TtrafficLightStatus x passedSec)
            |> Array.forall (fun x -> x = LightStatus.Green)
         then passedSec
         else if passedSec < 18001 then FindNextAllGreen periods (passedSec + 1)
         else raise (System.TimeoutException("Signals fail to synchronise in 5 hours"))
     
-    let rec private FindfirstNotAllGreenSec periods startSec = 
+    let rec private FindFirstNotAllGreenSec periods startSec = 
         if periods
-           |> Array.map (fun x -> trafficLight x startSec)
+           |> Array.map (fun x -> TtrafficLightStatus x startSec)
            |> Array.forall (fun x -> x = LightStatus.Green)
-        then FindfirstNotAllGreenSec periods (startSec + 1)
+        then FindFirstNotAllGreenSec periods (startSec + 1)
         else startSec
     
     let public GetAnswer input = 
@@ -45,7 +43,5 @@ module Q161FS =
             raise 
                 (System.ArgumentException
                      ("There will always be at least two signals in a scenario and never more than 100."))
-        let startSec = 0
-        let firstNotAllGreenSec = FindfirstNotAllGreenSec input startSec
-        let nextAllGreenSec = FindNextAllGreen input firstNotAllGreenSec
+        let nextAllGreenSec = FindFirstNotAllGreenSec input 0 |> FindNextAllGreen input
         new System.TimeSpan(0, 0, nextAllGreenSec - 1)
