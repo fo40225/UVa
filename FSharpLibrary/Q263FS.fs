b394cde10d9f3a01d9ac92208ec5ee9e8a87177c
fo40225 <fo40225@hotmail.com>
2015-05-23 17:55:31 +0800

    Q263 fsharp all test passed

--- FSharpLibrary/Q263FS.fs
+++ FSharpLibrary/Q263FS.fs
@@ -1,6 +1,36 @@
 ﻿namespace FSharpLibrary
 
-module Q263FS =
-    let public GetAnswer input =
-        failwith "Not implemented yet"
-        0
+module public Q263FS = 
+    let private GetMin input = 
+        string input
+        |> Seq.toArray
+        |> Array.map (fun x -> string x |> int)
+        |> Array.sort
+        |> Array.map string
+        |> String.concat System.String.Empty
+        |> int
+    
+    let private GetMax input = 
+        string input
+        |> Seq.toArray
+        |> Array.map (fun x -> string x |> int)
+        |> Array.sort
+        |> Array.rev
+        |> Array.map string
+        |> String.concat System.String.Empty
+        |> int
+    
+    let rec private Calc input set = 
+        let max = GetMax input
+        let min = GetMin input
+        let ans = max - min
+        printfn "%d - %d = %d" max min ans
+        if Set.contains ans set then Set.count set
+        else Set.add ans set |> Calc ans
+    
+    let public GetAnswer input = 
+        let set = Set.empty.Add input
+        printfn "Original number was %d" input
+        let chainLenght = Calc input set
+        printfn "Chain length %d" chainLenght
+        0 // return an integer exit code
