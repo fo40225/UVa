b74f6b62fa15e56d3ba6165ffb1c32ef792daed9
fo40225 <fo40225@hotmail.com>
2015-05-23 17:49:35 +0800

    Add Q263 unit test

---
+++ CSharpLibrary/Q263CS.cs
@@ -0,0 +1,22 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Q263CS.cs">
+//     Copyright (c) Ching Hsu. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace CSharpLibrary
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public static class Q263CS
+    {
+        public static void GetAnswer(int input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
