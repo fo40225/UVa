10e31c156150d1ddd37546f50e882ddb5c231b79
fo40225 <fo40225@hotmail.com>
2015-05-27 22:14:18 +0800

    Q10023 fsharp all test passed

--- FSharpLibrary/Q10023FS.fs
+++ FSharpLibrary/Q10023FS.fs
@@ -1,7 +1,43 @@
 ﻿namespace FSharpLibrary
 
-// Q10023 in F#
-module Q10023FS =
-    let public GetAnswer input =
-        failwith "Not implemented yet"
-        0
+// ----------------------------------------------------
+//                       result, p                     
+//                        v                            
+//                       3 0 7                         
+//                      ------                         
+//                  3   |94249 <- num (group by 100: 3)
+// d, guessResult ->3   |90000 <- value                
+//     (p*20+d)*d ->60  | 4249 <- test, next           
+//                   0  |  0                           
+//                  607 | 4249                         
+//                    7 | 4249                         
+// ----------------------------------------------------
+// Q10023 Square root in F#
+module Q10023FS = 
+    let public GetAnswer input = 
+        let rec GetSqrtRoot num times p = 
+            let rec Pow x y t = 
+                match y with
+                | 0 -> 1I
+                | 1 -> t
+                | _ -> Pow x (y - 1) (x * t)
+            
+            let rec guess digi = 
+                let d = bigint (int digi)
+                let value = (((20I * p) + d) * d) * (Pow 100I times 100I)
+                let test = num - value
+                if (digi <> 0) && (test < 0I) then guess (digi - 1)
+                else (((10I * p) + d), test)
+            
+            let guessResult = guess 9
+            let next = snd guessResult
+            let result = fst guessResult
+            if next = 0I || times = 0 then result * (Pow 10I times 10I)
+            else GetSqrtRoot next (times - 1) result
+        
+        let rec div100 input tail = 
+            if (input / 100I) = 0I then tail
+            else div100 (input / 100I) (tail + 1)
+        
+        let groups = div100 input 0
+        GetSqrtRoot input groups 0I
--- UnitTestProject/Q10023Tests.cs
+++ UnitTestProject/Q10023Tests.cs
@@ -4,21 +4,27 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
-// http: //uva.onlinejudge.org/external/100/10023.pdf Q10023 Square root _ You are to determinate X
-// by given Y , from expression X = √Y
+// http://uva.onlinejudge.org/external/100/10023.pdf
+// Q10023 Square root                                        _
+// You are to determinate X by given Y, from expression X = √Y
 
-// Input The first line is the number of test cases, followed by a blank line. Each test case of the
-// input contains a positive integer Y (1 ≤ Y ≤ 10^1000), with no blanks or leading zeroes in it. It
-// is guaranteed, that for given Y , X will be always an integer. Each test case will be separated
-// by a single line.
+// Input
+// The first line is the number of test cases, followed by a blank line.
+// Each test case of the input contains a positive integer Y (1 ≤ Y ≤ 10^1000), with no blanks or leading
+// zeroes in it.
+// It is guaranteed, that for given Y , X will be always an integer.
+// Each test case will be separated by a single line.
 
-// Output For each test case, your program should print X in the same format as Y was given in
-// input. Print a blank line between the outputs for two consecutive test cases.
+// Output
+// For each test case, your program should print X in the same format as Y was given in input.
+// Print a blank line between the outputs for two consecutive test cases.
 
-// Sample Input 1 7206604678144
+// Sample Input
+// 1
+// 7206604678144
 
-// Sample Output 2684512
-
+// Sample Output
+// 2684512
 namespace UnitTestProject
 {
     using System;
@@ -103,7 +109,8 @@ namespace UnitTestProject
             Assert.AreEqual(expected, actual);
         }
 
-        #endregion
+        #endregion CSharp
+
         #region FSharp
 
         [TestMethod]
@@ -176,6 +183,6 @@ namespace UnitTestProject
             Assert.AreEqual(expected, actual);
         }
 
-        #endregion
+        #endregion FSharp
     }
 }
